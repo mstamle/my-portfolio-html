31570c50c558da42504b62a9c89c2c289ea268c8
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-01-31 09:40:45 +0100

    Portfolio part updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,4 @@
-$('#mybtn').click(function(){
-    $('#square').fadeToggle(4000);
-});
 
-
-$('#mybtn2').click(function(){
-    $('#square').animate(
-        {'width':'toggle'},
-        2000
-    );
-});
-
-$('#mybtn3').click(function(){
-    $('#square').animate(
-        {'left':'+=200px'},
-        2000
-    );
-});
-
-$('#mybtn4').click(function(){
-    $('#square').animate(
-        {'left':'-=200px'},
-        2000
-    );
-});
-
-$('#my-btn').click(function(){
-    $('#bar1').animate({'width':'50%'}, 500);
-    $('#bar2').animate({'width':'75%'}, 500);
-    $('#bar3').animate({'width':'40%'}, 500);
-    $('#bar4').animate({'width':'95%'}, 500);
-});
-
-// At this point we see that the action is repetitive
-// We can create a function for that:
-// Also we can dictate it to appear on scroll:
 
 $('.myProgress').appear();
 
